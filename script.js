1fe9ba6a00802d768d6f87c69ba8d2247653ab64
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-21 08:31:39 +0200

    Color grid squares on hover

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 "use strict"
 
+// FUNCTIONS
 const createDiv = () => {
-    const container = document.querySelector(".container");
+    const grid = document.querySelector(".container");
     const div = document.createElement("div");
-    container.appendChild(div);
+    grid.appendChild(div);
 }
 
-const gridItems = 16 * 16;
+const addEventListeners = () => {
+    const gridItems = document.querySelectorAll(".container div");
+    gridItems.forEach(element => element.addEventListener("mouseenter", colorDiv));
+}
 
-for (let i = 0; i < gridItems; i++) {
+const colorDiv = (event) => {
+    const target = event.target;
+    target.classList.add("colored")
+}
+
+// MAIN
+const gridItemNumber = 16 * 16;
+for (let i = 0; i < gridItemNumber; i++) {
     createDiv();
 }
+addEventListeners();
