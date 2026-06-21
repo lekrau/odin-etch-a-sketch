54b64997bf59cb95ecf60ce3a3ca2929612d9c07
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-21 11:23:06 +0200

    Randomize square colors on first hover

--- script.js
+++ script.js
@@ -12,9 +12,18 @@ const addEventListeners = () => {
     gridItems.forEach(element => element.addEventListener("mouseenter", colorDiv));
 }
 
+const randomRGB = () => {
+    return Math.floor(Math.random() * (255 + 1));
+}
+
 const colorDiv = event => {
     const target = event.target;
-    target.classList.add("colored")
+    if (target.style.backgroundColor === "") {
+        const redValue = randomRGB();
+        const greenValue = randomRGB();
+        const blueValue = randomRGB();
+        target.style.backgroundColor = `rgb(${redValue},${greenValue},${blueValue})`;
+    }
 }
 
 const createGrid = () => {
@@ -61,3 +70,9 @@ createGrid(squaresPerSide);
 const button = document.querySelector("button");
 button.textContent = "New Grid";
 button.addEventListener("click", replaceGrid);
+
+// TODO
+// Transform the behavior of a square when interacting with the mouse by introducing a series of modifications.
+// 2. Additionally, implement a progressive darkening effect where each interaction darkens the square by 10%. The goal is to achieve a fully black (or completely colored) square in only ten interactions.
+// Hint: The opacity CSS property is useful here. To learn how to use it, check this MDN docs article about the opacity CSS property.
+// You can choose to do either one or both of these challenges, it’s up to you.
